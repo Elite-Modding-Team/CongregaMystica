86b84b478958661e07e456e40f0b6c3239c70219
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2025-07-22 16:09:02 -0700

    mod helpers

---
+++ src/main/java/congregamystica/utils/libs/ModIds.java
@@ -0,0 +1,43 @@
+package congregamystica.utils.libs;
+
+import congregamystica.utils.helpers.ModHelper;
+
+import javax.annotation.Nullable;
+
+public enum ModIds {
+    thaumic_wonders(ConstIds.thaumic_wonders, ConstVersions.thaumic_wonders, true, false);
+
+    public final String modId;
+    public final String version;
+    public final boolean isLoaded;
+
+    ModIds(String modId, @Nullable String version, boolean isMinVersion, boolean isMaxVersion) {
+        this.modId = modId;
+        this.version = version;
+        this.isLoaded = ModHelper.isModLoaded(modId, version, isMinVersion, isMaxVersion);
+    }
+
+    ModIds(String modId, @Nullable String version) {
+        this.modId = modId;
+        this.version = version;
+        this.isLoaded = ModHelper.isModLoaded(modId, version);
+    }
+
+    ModIds(String modId) {
+        this(modId, null);
+    }
+
+    @Override
+    public String toString() {
+        return this.modId;
+    }
+
+    public static class ConstIds {
+        public static final String thaumcraft = "thaumcraft";
+        public static final String thaumic_wonders = "thaumicwonders";
+    }
+
+    public static class ConstVersions {
+        public static final String thaumic_wonders = "2.0.0";
+    }
+}

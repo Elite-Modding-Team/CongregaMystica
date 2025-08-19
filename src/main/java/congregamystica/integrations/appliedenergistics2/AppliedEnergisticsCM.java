992689b22e6a45b9d27391cf72f507d725136c3b
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2025-08-19 01:37:47 -0700

    ae2 oredict workaround

--- src/main/java/congregamystica/integrations/appliedenergistics2/AppliedEnergisticsCM.java
+++ src/main/java/congregamystica/integrations/appliedenergistics2/AppliedEnergisticsCM.java
@@ -3,6 +3,8 @@ package congregamystica.integrations.appliedenergistics2;
 import congregamystica.api.IProxy;
 import congregamystica.utils.helpers.RegistryHelper;
 import congregamystica.utils.libs.ModIds;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -14,8 +16,16 @@ public class AppliedEnergisticsCM implements IProxy {
     public void init() {
         Item ae2Material = RegistryHelper.getRegisteredItem(new ResourceLocation(ModIds.ConstIds.applied_energistics, "material"));
         if(ae2Material != Items.AIR) {
-            OreDictionary.registerOre("gemCertusQuartz", new ItemStack(ae2Material, 1, 0));
-            OreDictionary.registerOre("gemChargedCerusQuartz", new ItemStack(ae2Material, 1, 1));
+            OreDictionary.registerOre("gemQuartzCertus", new ItemStack(ae2Material, 1, 0));
+            OreDictionary.registerOre("gemChargedQuartzCertus", new ItemStack(ae2Material, 1, 1));
+        }
+        Block certusOre = RegistryHelper.getRegisteredBlock(new ResourceLocation(ModIds.ConstIds.applied_energistics, "quartz_ore"));
+        if(certusOre != Blocks.AIR) {
+            OreDictionary.registerOre("oreQuartzCertus", certusOre);
+        }
+        Block chargedCertusOre = RegistryHelper.getRegisteredBlock(new ResourceLocation(ModIds.ConstIds.applied_energistics, "charged_quartz_ore"));
+        if(chargedCertusOre != Blocks.AIR) {
+            OreDictionary.registerOre("oreChargedQuartzCertus", chargedCertusOre);
         }
     }
 }

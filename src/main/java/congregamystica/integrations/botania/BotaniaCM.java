4833aba26e06c21171ac4c7b156fa41cfd9b4197
Me <135455255+IcarussOne@users.noreply.github.com>
2025-08-22 04:38:31 -0500

    start tainthistle

--- src/main/java/congregamystica/integrations/botania/BotaniaCM.java
+++ src/main/java/congregamystica/integrations/botania/BotaniaCM.java
@@ -2,16 +2,20 @@ package congregamystica.integrations.botania;
 
 import congregamystica.api.IProxy;
 import congregamystica.integrations.botania.blocks.BlockSpecialFlowerCM;
+import congregamystica.integrations.botania.blocks.subtiles.SubTileTainthistle;
 import congregamystica.integrations.botania.blocks.subtiles.SubTileWhisperweed;
 import congregamystica.registry.RegistrarCM;
 
 public class BotaniaCM implements IProxy {
+    public static final String TAINTHISTLE = "tainthistle";
     public static final String WHISPERWEED = "whisperweed";
 
+    public static BlockSpecialFlowerCM<SubTileTainthistle> tainthistle;
     public static BlockSpecialFlowerCM<SubTileWhisperweed> whisperweed;
 
     @Override
     public void preInit() {
+        RegistrarCM.addAdditionToRegister(tainthistle = new BlockSpecialFlowerCM<>(new SubTileTainthistle(), TAINTHISTLE));
         RegistrarCM.addAdditionToRegister(whisperweed = new BlockSpecialFlowerCM<>(new SubTileWhisperweed(), WHISPERWEED));
     }
 }

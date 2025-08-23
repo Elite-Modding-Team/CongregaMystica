92db258073677a15aab3ed1a6d78adbda7a20369
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2025-08-23 12:41:40 -0700

    improved Mimic Fork handling

--- src/main/java/congregamystica/integrations/congregamystica/items/ItemMimicFork.java
+++ src/main/java/congregamystica/integrations/congregamystica/items/ItemMimicFork.java
@@ -6,11 +6,9 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.SoundEvents;
-import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.nbt.NBTTagCompound;
@@ -41,94 +39,48 @@ public class ItemMimicFork extends AbstractItemAddition {
     }
 
     @Override
-    public void registerRecipe(IForgeRegistry<IRecipe> registry) {
-        //TODO: Add recipe
-    }
-
-    @Override
-    public void registerResearchLocation() {
-        //TODO: Add research
-    }
-
-    @Override
-    public void registerAspects(AspectEventProxy registry, Map<ItemStack, AspectList> aspectMap) {
-        //TODO: Aspects may not be needed depending on the crafting recipe
-    }
-
-    @Override
-    public boolean isEnabled() {
-        //Mimic fork is always enabled
-        return true;
-    }
-
-    @Override
     public void onCreated(@NotNull ItemStack stack, @NotNull World world, @NotNull EntityPlayer player) {
         super.onCreated(stack, world, player);
-
-        NBTTagCompound tagCompound = new NBTTagCompound();
-        tagCompound.setByte("note", (byte) 1);
-        tagCompound.setInteger("tone", 1);
-
-        stack.setTagCompound(tagCompound);
+        this.initNoteTags(stack);
     }
 
     @Override
     @SideOnly(Side.CLIENT)
     public void addInformation(@NotNull ItemStack stack, @Nullable World world, @NotNull List<String> tooltip, @NotNull ITooltipFlag flag) {
-        super.addInformation(stack, world, tooltip, flag);
-
-        if (stack.hasTagCompound() && stack.getTagCompound() != null) {
-            NBTTagCompound tagCompound = stack.getTagCompound();
-
-            if (tagCompound.hasKey("tone"))
-                tooltip.add(I18n.format("mimic_fork.instrument", I18n.format("mimic_fork.instrument." + getInstrumentNameFromInt(tagCompound.getInteger("tone")))));
-            if (tagCompound.hasKey("note")) {
-                tooltip.add(I18n.format("mimic_fork.note", tagCompound.getByte("note")));
-            }
-        }
+        int tone = this.getTone(stack);
+        byte note = this.getNote(stack);
+        tooltip.add(I18n.format("mimic_fork.instrument", I18n.format("mimic_fork.instrument." + getInstrumentNameFromInt(tone))));
+        tooltip.add(I18n.format("mimic_fork.note", note));
     }
 
     @Override
-    public @NotNull EnumActionResult onItemUseFirst(EntityPlayer player, @NotNull World world, @NotNull BlockPos pos, @NotNull EnumFacing side, float hitX, float hitY, float hitZ, @NotNull EnumHand hand) {
+    public @NotNull EnumActionResult onItemUseFirst(EntityPlayer player, @NotNull World world, @NotNull BlockPos pos, @NotNull EnumFacing side, float hitX, float hitY, float hitZ, @NotNull EnumHand hand)  {
         ItemStack heldStack = player.getHeldItem(hand);
-        NBTTagCompound tagCompound = heldStack.getTagCompound();
+        TileEntity tile = world.getTileEntity(pos);
 
-        if (tagCompound == null) {
-            tagCompound = new NBTTagCompound();
-            heldStack.setTagCompound(tagCompound);
-        }
-
-        TileEntity tile;
-        tile = world.getTileEntity(pos);
-
-        if (!world.isRemote && player.isSneaking()) {
-
+        if(player.isSneaking()) {
             if (tile instanceof TileEntityNote) {
-                IBlockState iblockstate = world.getBlockState(pos.down());
-
-                tagCompound.setByte("note", ((TileEntityNote) tile).note);
-                tagCompound.setInteger("tone", getToneFromState(iblockstate));
-
+                IBlockState down = world.getBlockState(pos.down());
+                this.setNote(heldStack, ((TileEntityNote) tile).note);
+                this.setTone(heldStack, getToneFromState(down));
                 return EnumActionResult.SUCCESS;
             } else if (tile instanceof TileArcaneEar) {
-                tagCompound.setByte("note", ((TileArcaneEar) tile).note);
-                tagCompound.setInteger("tone", ((TileArcaneEar) tile).tone);
+                this.setNote(heldStack, ((TileArcaneEar) tile).note);
+                this.setTone(heldStack, ((TileArcaneEar) tile).tone);
+                return EnumActionResult.SUCCESS;
             }
-        } else if (!world.isRemote && !player.isSneaking()) {
+        } else {
             if (tile instanceof TileArcaneEar) {
-                ((TileArcaneEar) tile).note = tagCompound.getByte("note");
+                ((TileArcaneEar) tile).note = this.getNote(heldStack);
                 tile.markDirty();
                 return EnumActionResult.SUCCESS;
             }
-
             if (tile instanceof TileEntityNote) {
-                ((TileEntityNote) tile).note = tagCompound.getByte("note");
+                ((TileEntityNote) tile).note = this.getNote(heldStack);
                 tile.markDirty();
                 return EnumActionResult.SUCCESS;
             }
         }
-
-
         return EnumActionResult.PASS;
     }
 
@@ -138,39 +90,24 @@ public class ItemMimicFork extends AbstractItemAddition {
 
         if (material == Material.ROCK) {
             i = 1;
-        }
-
-        if (material == Material.SAND) {
+        } else if (material == Material.SAND) {
             i = 2;
-        }
-
-        if (material == Material.GLASS) {
+        } else if (material == Material.GLASS) {
             i = 3;
-        }
-
-        if (material == Material.WOOD) {
+        } else if (material == Material.WOOD) {
             i = 4;
         }
 
         Block block = state.getBlock();
-
         if (block == Blocks.CLAY) {
             i = 5;
-        }
-
-        if (block == Blocks.GOLD_BLOCK) {
+        } else if (block == Blocks.GOLD_BLOCK) {
             i = 6;
-        }
-
-        if (block == Blocks.WOOL) {
+        } else if (block == Blocks.WOOL) {
             i = 7;
-        }
-
-        if (block == Blocks.PACKED_ICE) {
+        } else if (block == Blocks.PACKED_ICE) {
             i = 8;
-        }
-
-        if (block == Blocks.BONE_BLOCK) {
+        } else if (block == Blocks.BONE_BLOCK) {
             i = 9;
         }
 
@@ -179,40 +116,49 @@ public class ItemMimicFork extends AbstractItemAddition {
 
     @Override
     public @NotNull ActionResult<ItemStack> onItemRightClick(@NotNull World world, EntityPlayer player, @NotNull EnumHand hand) {
-        ItemStack itemstack = player.getHeldItem(hand);
-        player.setActiveHand(hand);
-        return new ActionResult<>(EnumActionResult.SUCCESS, itemstack);
-    }
-
-    @Override
-    public @NotNull EnumAction getItemUseAction(@NotNull ItemStack stack) {
-        return EnumAction.BOW;
-    }
-
-    @Override
-    public int getMaxItemUseDuration(@NotNull ItemStack stack) {
-        return 100;
-    }
-
-    @Override
-    public void onPlayerStoppedUsing(ItemStack stack, @NotNull World world, @NotNull EntityLivingBase entity, int timeLeft) {
-        if (stack.hasTagCompound()) {
-            NBTTagCompound tagCompound = stack.getTagCompound();
-
-            int note = tagCompound.getByte("note");
-            int tone = tagCompound.getInteger("tone");
-
-            NoteBlockEvent.Play event = new NoteBlockEvent.Play(world, entity.getPosition(), null, note, tone);
-
+        if(!player.isSneaking()) {
+            ItemStack heldStack = player.getHeldItem(hand);
+            int note = getNote(heldStack);
+            int tone = getTone(heldStack);
+            NoteBlockEvent.Play event = new NoteBlockEvent.Play(world, player.getPosition(), null, note, tone);
             if (!MinecraftForge.EVENT_BUS.post(event)) {
-                float f = (float) Math.pow(2.0D, (note - 12) / 12.0D);
-
-
-                world.playSound(null, entity.getPosition().up(1), getInstrumentSound(event.getInstrument()), SoundCategory.PLAYERS, 3.0F, f);
-                world.spawnParticle(EnumParticleTypes.NOTE, entity.posX + 0.5D, entity.posY + 0.5D, entity.posZ + 0.5D, (double) note / 24.0D, 0.0D, 0.0D);
+                float pitch = (float) Math.pow(2.0D, (note - 12) / 12.0D);
+                world.playSound(null, player.getPosition().up(1), getInstrumentSound(event.getInstrument()), SoundCategory.PLAYERS, 3.0F, pitch);
+                world.spawnParticle(EnumParticleTypes.NOTE, player.posX + 0.5D, player.posY + 0.5D, player.posZ + 0.5D, (double) note / 24.0D, 0.0D, 0.0D);
+                player.swingArm(hand);
+                return new ActionResult<>(EnumActionResult.SUCCESS, heldStack);
             }
-
         }
+        return super.onItemRightClick(world, player, hand);
+    }
+
+    private void initNoteTags(ItemStack stack) {
+        this.setNote(stack, (byte) 0);
+        this.setTone(stack, 0);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    private void setNote(ItemStack stack, byte note) {
+        if(!stack.hasTagCompound()) {
+            stack.setTagCompound(new NBTTagCompound());
+        }
+        stack.getTagCompound().setByte("note", note);
+    }
+
+    private byte getNote(ItemStack stack) {
+        return stack.hasTagCompound() && stack.getTagCompound() != null ? stack.getTagCompound().getByte("note") : 0;
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    private void setTone(ItemStack stack, int tone) {
+        if(!stack.hasTagCompound()) {
+            stack.setTagCompound(new NBTTagCompound());
+        }
+        stack.getTagCompound().setInteger("tone", tone);
+    }
+
+    private int getTone(ItemStack stack) {
+        return stack.hasTagCompound() && stack.getTagCompound() != null ? stack.getTagCompound().getByte("tone") : 0;
     }
 
     private String getInstrumentNameFromInt(int toneIn) {
@@ -242,5 +188,29 @@ public class ItemMimicFork extends AbstractItemAddition {
             default:
                 return SoundEvents.BLOCK_NOTE_BASS;
         }
+    }
+
+    //##########################################################
+    // IItemAddition
+
+    @Override
+    public void registerRecipe(IForgeRegistry<IRecipe> registry) {
+        //TODO: Add recipe
+    }
+
+    @Override
+    public void registerResearchLocation() {
+        //TODO: Add research
+    }
+
+    @Override
+    public void registerAspects(AspectEventProxy registry, Map<ItemStack, AspectList> aspectMap) {
+        //TODO: Aspects may not be needed depending on the crafting recipe
+    }
+
+    @Override
+    public boolean isEnabled() {
+        //Mimic fork is always enabled
+        return true;
     }
 }

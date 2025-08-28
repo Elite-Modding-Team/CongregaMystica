e6279ad0b3b9cec9a2e38f0bb98052f0e361864a
Me <135455255+IcarussOne@users.noreply.github.com>
2025-08-28 07:15:13 -0500

    add ironwood golem and research

--- src/main/java/congregamystica/integrations/rustic/golems/GolemMaterialIronwood.java
+++ src/main/java/congregamystica/integrations/rustic/golems/GolemMaterialIronwood.java
@@ -3,13 +3,19 @@ package congregamystica.integrations.rustic.golems;
 import congregamystica.CongregaMystica;
 import congregamystica.api.golem.IGolemAddition;
 import congregamystica.config.ConfigHandlerCM;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.oredict.OreDictionary;
 import rustic.common.blocks.ModBlocks;
+import thaumcraft.api.ThaumcraftApi;
 import thaumcraft.api.golems.EnumGolemTrait;
 import thaumcraft.api.golems.parts.GolemMaterial;
 import thaumcraft.api.items.ItemsTC;
+import thaumcraft.api.research.ScanBlockState;
+import thaumcraft.api.research.ScanItem;
+import thaumcraft.api.research.ScanningManager;
 
 public class GolemMaterialIronwood implements IGolemAddition {
     @Override
@@ -25,12 +31,10 @@ public class GolemMaterialIronwood implements IGolemAddition {
 
     @Override
     public void registerGolemMaterial() {
-        //TODO: Research
-        String[] research = new String[0];
         GolemMaterial.register(new GolemMaterial(
                 this.getGolemMaterialKey(),
-                research,
-                new ResourceLocation(CongregaMystica.MOD_ID, ""),
+                new String[]{"CM_GOLEM_MAT_IRONWOOD"},
+                new ResourceLocation(CongregaMystica.MOD_ID, "textures/entity/golem/rustic/mat_ironwood.png"),
                 11045996,
                 ConfigHandlerCM.golems.ironWood.statHealth,
                 ConfigHandlerCM.golems.ironWood.statArmor,
@@ -41,13 +45,22 @@ public class GolemMaterialIronwood implements IGolemAddition {
         ));
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public void registerResearchLocation() {
-
+    	// TODO: Ironwood planks need a unique ore dictionary
+        ItemStack stack = ConfigHandlerCM.golems.ironWood.getMaterialStack();
+        Block block = Block.getBlockFromItem(stack.getItem());
+        if (block != Blocks.AIR) {
+            ScanningManager.addScannableThing(new ScanBlockState("f_CM_IRONWOOD", block.getStateFromMeta(stack.getMetadata()), false));
+        }
+        ScanningManager.addScannableThing(new ScanItem("f_CM_IRONWOOD", stack));
+        ThaumcraftApi.registerResearchLocation(new ResourceLocation(CongregaMystica.MOD_ID,
+                "research/rustic/golem_mat_ironwood"));
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return ConfigHandlerCM.golems.ironWood.enable;
     }
 }

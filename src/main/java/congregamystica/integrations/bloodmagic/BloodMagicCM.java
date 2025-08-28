44e5ea5451c36784689bd1717cb9580b64cf30b1
Me <135455255+IcarussOne@users.noreply.github.com>
2025-08-28 08:17:30 -0500

    move blood magic research

--- src/main/java/congregamystica/integrations/bloodmagic/BloodMagicCM.java
+++ src/main/java/congregamystica/integrations/bloodmagic/BloodMagicCM.java
@@ -1,14 +1,19 @@
 package congregamystica.integrations.bloodmagic;
 
+import congregamystica.CongregaMystica;
 import congregamystica.api.IProxy;
 import congregamystica.integrations.bloodmagic.additions.BloodOrbCM;
 import congregamystica.integrations.bloodmagic.items.ItemBloodScribingTools;
 import congregamystica.integrations.bloodmagic.items.ItemBoundCaster;
 import congregamystica.registry.RegistrarCM;
+import net.minecraft.util.ResourceLocation;
+import thaumcraft.api.ThaumcraftApi;
 
 public class BloodMagicCM implements IProxy {
     @Override
     public void preInit() {
+        ThaumcraftApi.registerResearchLocation(new ResourceLocation(CongregaMystica.MOD_ID, "research/bloodmagic/blood_magic"));
+
         RegistrarCM.addAdditionToRegister(new ItemBoundCaster());
         RegistrarCM.addAdditionToRegister(new BloodOrbCM());
         RegistrarCM.addAdditionToRegister(new ItemBloodScribingTools());

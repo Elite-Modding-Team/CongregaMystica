3183d1d4264a54d8fbf883012a92ff13931f49e5
Me <135455255+IcarussOne@users.noreply.github.com>
2025-10-26 19:38:31 -0500

    add goggled thaumium helmet
    I just think it's fashionable.

--- src/main/java/congregamystica/registry/ModItemsCM.java
+++ src/main/java/congregamystica/registry/ModItemsCM.java
@@ -13,6 +13,8 @@ import java.util.List;
 @GameRegistry.ObjectHolder(CongregaMystica.MOD_ID)
 public class ModItemsCM {
     //Congrega Mystica
+    public static final Item FLUX_SCRIBING_TOOLS = null;
+    public static final Item GOGGLED_THAUMIUM_HELMET = null;
     public static final Item MIMIC_FORK = null;
     public static final Item MIMIC_FORK_RANGED = null;
 
